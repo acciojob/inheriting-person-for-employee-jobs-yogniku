3af4453a39654a528a20590c662712dc8eafed9e
yogniku <156093318+yogniku@users.noreply.github.com>
2024-03-12 18:00:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // complete this js code
 function Person(name, age) {
+	this.pername=name;
+	this.pernage=age;
 	greet(){
 		console.log(Hello, my name is ${this.name}, I am ${this.age}years old.)
 	}
 }
 greet();
 function Employee(name, age,jobTitle) {
+	this.empname=name;
+	this.empage=age;
+	this.empjobtitle=jobTitle;
 	jobGreet(){
 		console.log(Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle})
 	}
