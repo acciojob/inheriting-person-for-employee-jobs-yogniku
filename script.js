c3b9d331b152b007b5ce554754b0272d0d926882
yogniku <156093318+yogniku@users.noreply.github.com>
2024-03-18 17:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 // complete this js code
-function Person(name, age) {
-	this.pername=name;
-	this.pernage=age;
-	greet(){
-		console.log(Hello, my name is ${this.name}, I am ${this.age}years old.)
-	}
-}
-greet();
-function Employee(name, age,jobTitle) {
-	this.empname=name;
-	this.empage=age;
+
+class Person {
+    constructor(name,age) {
+        this.pername=name;
+        this.pernage=age;
+    }
+    jobGreet() {
+      return `Hello, my name is ${this.pername}, I am ${this.pernage}years old.`
+    }
+  }
+  
+  class Employee extends Person {
+    constructor(name, age,jobTitle) {
+      super(name);
+    this.empage=age;
 	this.empjobtitle=jobTitle;
-	jobGreet(){
-		console.log(Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle})
-	}
-}
-jobGreet()
+    }
+    greet() {
+      return `Hello,my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`
+    }
+  }
+  
+  const myPerson  = new Person("Alice",25);
+ //document.getElementById("demo").innerHTML = myPerson.greet();
+//const Person=new Person(Alice,25)
+
+//const Employee=new Employee()
+//Alice" and age 25
 // Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+ window.Person = Person;
+ window.Employee = Employee;
